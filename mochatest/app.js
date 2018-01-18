a9d878776fbd025df5a5e08e2469b78c94813126
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-18 17:51:41 -0600

    Create a test case with isAbove

--- mochatest/app.js
+++ mochatest/app.js
@@ -1,5 +1,8 @@
 module.exports = {
-  sayHello: ()=> {
+  sayHello: () => {
     return 'hello';
-  }
+  },
+  addNumbers: ((value1, value2) => {
+    return value1 + value2;
+  })
 };
--- mochatest/test/appTest.js
+++ mochatest/test/appTest.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
+const addNumbers = require('../app').addNumbers;
 
 describe('App', function() {
   it('sayHello should return hello', function() {
@@ -7,8 +8,13 @@ describe('App', function() {
     assert.equal(result, 'hello');
   });
 
-  it('sayHello should return type string', ()=> {
+  it('sayHello should return type string', () => {
     let result = sayHello();
     assert.typeOf(result, 'string');
+  });
+
+  it('addNumbers should be above 5', () => {
+    let result = addNumbers(5, 5);
+    assert.isAbove(result, 5)
   });
 });
