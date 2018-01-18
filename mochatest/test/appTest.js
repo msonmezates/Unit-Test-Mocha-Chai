39310229bb0be3b750cdf9f2b4ddfa2d4fcbf7c8
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-01-18 17:39:49 -0600

    Add a test case with typeOf

--- mochatest/test/appTest.js
+++ mochatest/test/appTest.js
@@ -2,8 +2,13 @@ const assert = require('chai').assert;
 const sayHello = require('../app').sayHello;
 
 describe('App', function() {
-  it('app should return hello', function() {
+  it('sayHello should return hello', function() {
     let result = sayHello();
-    assert.equal(result, 'hello');  
+    assert.equal(result, 'hello');
+  });
+
+  it('sayHello should return type string', ()=> {
+    let result = sayHello();
+    assert.typeOf(result, 'string');
   });
 });
