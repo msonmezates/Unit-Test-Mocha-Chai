b591a111f2a92551912e5994cc56e603889c643e
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-02-02 07:44:28 -0600

    Create actions and reducers and finish writing test cases for both

--- Mocha_Tests/src/components/comment_box.js
+++ Mocha_Tests/src/components/comment_box.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class CommentBox extends Component {
+import * as actions from '../actions';
+
+class CommentBox extends Component {
   constructor(props) {
     super(props);
 
@@ -15,6 +18,8 @@ export default class CommentBox extends Component {
 
   handleSubmit(event) {
     event.preventDefault();
+
+    this.props.saveComment(this.state.comment);
     this.setState({ comment: '' })
   }
 
@@ -24,12 +29,22 @@ export default class CommentBox extends Component {
         className="comment-box"
         onSubmit={this.handleSubmit.bind(this)}
       >
+        <h4>Add a comment</h4>
         <textarea
           value={this.state.comment}
           onChange={this.handleChange.bind(this)}
         />
-        <button action="submit">Submit Comment</button>
+        <div>
+          <button
+            className="btn btn-danger"
+            action="submit"
+          >
+            Submit Comment
+          </button>
+        </div>
       </form>
     );
   }
 }
+
+export default connect(null, actions)(CommentBox);
